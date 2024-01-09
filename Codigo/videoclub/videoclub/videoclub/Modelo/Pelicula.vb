86eac3235fa04db2bb19feb0ecd7b7ea2ca3a5c2
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-01-10 00:31:34 +0100

    Add ID Value

--- Codigo/videoclub/videoclub/videoclub/Modelo/Pelicula.vb
+++ Codigo/videoclub/videoclub/videoclub/Modelo/Pelicula.vb
@@ -1,4 +1,6 @@
 ﻿Public Class Pelicula
+
+    Property id As Integer
     Property director As Persona
     Property duracion As Integer
     Property productora As String
@@ -28,5 +30,18 @@
         Me.sinopsis = sinopsis
     End Sub
 
+    Public Sub New(id As Integer, director As Persona, duracion As Integer, productora As String, genero As Controller.genero_Enum.genero, sinopsis As String)
+        Me.id = id
+        Me.director = director
+        Me.duracion = duracion
+        Me.productora = productora
+        Me.genero = genero
+        Me.sinopsis = sinopsis
+    End Sub
 
+    Public Sub New(id As Integer, director As Persona, duracion As Integer, productora As String, genero As Controller.genero_Enum.genero, sinopsis As String, poster As Image, personajes As List(Of Personaje))
+        Me.New(id, director, duracion, productora, genero, sinopsis)
+        Me.poster = poster
+        Me.personajes = personajes
+    End Sub
 End Class
--- Codigo/videoclub/videoclub/videoclub/Modelo/Persona.vb
+++ Codigo/videoclub/videoclub/videoclub/Modelo/Persona.vb
@@ -1,9 +1,13 @@
 ﻿Public Class Persona
+    Property id As Integer
     Property nombre As String
 
     Public Sub New(nombre As String)
         Me.nombre = nombre
     End Sub
 
-
+    Public Sub New(id As Integer, nombre As String)
+        Me.id = id
+        Me.nombre = nombre
+    End Sub
 End Class
