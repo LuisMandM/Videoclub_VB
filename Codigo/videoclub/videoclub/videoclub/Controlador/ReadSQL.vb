bfef92404661495dff3973600f4131e1fdc5d086
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-01-10 19:03:11 +0100

    Add Reading Class

---
+++ Codigo/videoclub/videoclub/videoclub/Controlador/ReadSQL.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Data.SQLite
+Imports System.IO
+
+Public Class ReadSQL
+
+    Function ReadDirector_Single(id As Integer)
+        Dim resultado As Persona
+        Try
+            Dim conexion As SQLiteConnection = New SQLiteConnection(My.Settings.conexion_db)
+            Dim consulta As String = String.Format("SELECT ID, NOMBRE FROM DIRECTORES WHERE ID = @id”)
+            conexion.Open()
+            Dim cmd As New SQLiteCommand(consulta, conexion)
+            cmd.Parameters.Add("@id", DbType.Int64).Value = id
+            Dim lector As SQLiteDataReader = cmd.ExecuteReader()
+            While lector.Read()
+                resultado = New Persona(id:=lector.GetInt16(0), nombre:=lector.GetString(1))
+            End While
+            lector.Close()
+            conexion.Close()
+            Return resultado
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        End Try
+
+    End Function
+
+    Function ReadDirector_All()
+        Dim resultado As New List(Of Persona)
+        Try
+            Dim conexion As SQLiteConnection = New SQLiteConnection(My.Settings.conexion_db)
+            Dim consulta As String = String.Format("SELECT ID, NOMBRE FROM DIRECTORES”)
+            conexion.Open()
+            Dim cmd As New SQLiteCommand(consulta, conexion)
+            Dim lector As SQLiteDataReader = cmd.ExecuteReader()
+            While lector.Read()
+                Dim current = New Persona(id:=lector.GetInt16(0), nombre:=lector.GetString(1))
+                resultado.Add(current)
+            End While
+            lector.Close()
+            conexion.Close()
+            Return resultado
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Function
+
+
+End Class
