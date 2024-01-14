9d8e5c2be526183eea0504a140213fa4df5e8f70
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-01-14 12:52:48 +0100

    Adding Form Creation Persona

---
+++ Codigo/videoclub/videoclub/videoclub/Vista/Add_Persona_Form.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Add_Persona_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.lbl_Nombre = New System.Windows.Forms.Label()
+        Me.tbox_Name = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.lbl_Nombre, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.tbox_Name, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 2)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(452, 176)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'lbl_Nombre
+        '
+        Me.lbl_Nombre.AutoSize = True
+        Me.lbl_Nombre.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lbl_Nombre.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Nombre.Location = New System.Drawing.Point(3, 52)
+        Me.lbl_Nombre.Name = "lbl_Nombre"
+        Me.lbl_Nombre.Size = New System.Drawing.Size(220, 28)
+        Me.lbl_Nombre.TabIndex = 0
+        Me.lbl_Nombre.Text = "Label1"
+        Me.lbl_Nombre.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'tbox_Name
+        '
+        Me.tbox_Name.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbox_Name.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbox_Name.Location = New System.Drawing.Point(229, 55)
+        Me.tbox_Name.Name = "tbox_Name"
+        Me.tbox_Name.Size = New System.Drawing.Size(220, 22)
+        Me.tbox_Name.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Location = New System.Drawing.Point(3, 83)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(220, 38)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Add_Persona_Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(452, 176)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Add_Persona_Form"
+        Me.Text = "Add_Persona_Form"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents lbl_Nombre As Label
+    Friend WithEvents tbox_Name As TextBox
+    Friend WithEvents Button1 As Button
+End Class
--- Codigo/videoclub/videoclub/videoclub/Vista/writable_Form.Designer.vb
+++ Codigo/videoclub/videoclub/videoclub/Vista/writable_Form.Designer.vb
@@ -25,13 +25,17 @@ Partial Class writable_Form
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(writable_Form))
         Me.tlp_Principal = New System.Windows.Forms.TableLayoutPanel()
         Me.tlp_DisplayContent = New System.Windows.Forms.TableLayoutPanel()
-        Me.WritableContent1 = New WritableContentControl.WritableContent()
+        Me.wcontent = New WritableContentControl.WritableContent()
         Me.tlp_Buttons = New System.Windows.Forms.TableLayoutPanel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btton_add_Director = New System.Windows.Forms.Button()
+        Me.btton_add_Personaje = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btton_Save = New System.Windows.Forms.Button()
+        Me.btton_Discard = New System.Windows.Forms.Button()
         Me.tlp_Principal.SuspendLayout()
         Me.tlp_DisplayContent.SuspendLayout()
         Me.tlp_Buttons.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'tlp_Principal
@@ -40,6 +44,7 @@ Partial Class writable_Form
         Me.tlp_Principal.ColumnCount = 1
         Me.tlp_Principal.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.tlp_Principal.Controls.Add(Me.tlp_DisplayContent, 0, 0)
+        Me.tlp_Principal.Controls.Add(Me.TableLayoutPanel1, 0, 1)
         Me.tlp_Principal.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tlp_Principal.Location = New System.Drawing.Point(0, 0)
         Me.tlp_Principal.Name = "tlp_Principal"
@@ -54,37 +59,36 @@ Partial Class writable_Form
         Me.tlp_DisplayContent.ColumnCount = 2
         Me.tlp_DisplayContent.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.0!))
         Me.tlp_DisplayContent.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.0!))
-        Me.tlp_DisplayContent.Controls.Add(Me.WritableContent1, 0, 0)
+        Me.tlp_DisplayContent.Controls.Add(Me.wcontent, 0, 0)
         Me.tlp_DisplayContent.Controls.Add(Me.tlp_Buttons, 1, 0)
         Me.tlp_DisplayContent.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tlp_DisplayContent.Location = New System.Drawing.Point(3, 3)
         Me.tlp_DisplayContent.Name = "tlp_DisplayContent"
         Me.tlp_DisplayContent.RowCount = 1
         Me.tlp_DisplayContent.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.tlp_DisplayContent.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.tlp_DisplayContent.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 443.0!))
         Me.tlp_DisplayContent.Size = New System.Drawing.Size(1266, 443)
         Me.tlp_DisplayContent.TabIndex = 0
         '
-        'WritableContent1
+        'wcontent
         '
-        Me.WritableContent1.BackColor = System.Drawing.SystemColors.ControlText
-        Me.WritableContent1.DataSource = Nothing
-        Me.WritableContent1.DisplayMember1 = ""
-        Me.WritableContent1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.WritableContent1.Location = New System.Drawing.Point(3, 3)
-        Me.WritableContent1.MinimumSize = New System.Drawing.Size(317, 440)
-        Me.WritableContent1.Name = "WritableContent1"
-        Me.WritableContent1.Size = New System.Drawing.Size(880, 440)
-        Me.WritableContent1.TabIndex = 0
-        Me.WritableContent1.ValueMember1 = ""
+        Me.wcontent.BackColor = System.Drawing.SystemColors.ControlText
+        Me.wcontent.DataSource = Nothing
+        Me.wcontent.DisplayMember1 = ""
+        Me.wcontent.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wcontent.Location = New System.Drawing.Point(3, 3)
+        Me.wcontent.MinimumSize = New System.Drawing.Size(317, 440)
+        Me.wcontent.Name = "wcontent"
+        Me.wcontent.Size = New System.Drawing.Size(880, 440)
+        Me.wcontent.TabIndex = 0
+        Me.wcontent.ValueMember1 = ""
         '
         'tlp_Buttons
         '
         Me.tlp_Buttons.ColumnCount = 1
         Me.tlp_Buttons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.tlp_Buttons.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.tlp_Buttons.Controls.Add(Me.Button3, 0, 2)
-        Me.tlp_Buttons.Controls.Add(Me.Button1, 0, 1)
+        Me.tlp_Buttons.Controls.Add(Me.btton_add_Director, 0, 2)
+        Me.tlp_Buttons.Controls.Add(Me.btton_add_Personaje, 0, 1)
         Me.tlp_Buttons.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tlp_Buttons.Location = New System.Drawing.Point(889, 3)
         Me.tlp_Buttons.Name = "tlp_Buttons"
@@ -96,37 +100,78 @@ Partial Class writable_Form
         Me.tlp_Buttons.Size = New System.Drawing.Size(374, 437)
         Me.tlp_Buttons.TabIndex = 1
         '
-        'Button1
+        'btton_add_Director
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.ControlText
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button1.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.Color.Yellow
-        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
-        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button1.Location = New System.Drawing.Point(3, 112)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(368, 103)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Añadir Personaje"
-        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.btton_add_Director.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btton_add_Director.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btton_add_Director.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btton_add_Director.ForeColor = System.Drawing.Color.Yellow
+        Me.btton_add_Director.Image = CType(resources.GetObject("btton_add_Director.Image"), System.Drawing.Image)
+        Me.btton_add_Director.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btton_add_Director.Location = New System.Drawing.Point(3, 221)
+        Me.btton_add_Director.Name = "btton_add_Director"
+        Me.btton_add_Director.Size = New System.Drawing.Size(368, 103)
+        Me.btton_add_Director.TabIndex = 2
+        Me.btton_add_Director.Text = "Añadir Director"
+        Me.btton_add_Director.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btton_add_Director.UseVisualStyleBackColor = False
         '
-        'Button3
+        'btton_add_Personaje
         '
-        Me.Button3.BackColor = System.Drawing.SystemColors.ControlText
-        Me.Button3.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button3.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.ForeColor = System.Drawing.Color.Yellow
-        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
-        Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button3.Location = New System.Drawing.Point(3, 221)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(368, 103)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Añadir Director"
-        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.Button3.UseVisualStyleBackColor = False
+        Me.btton_add_Personaje.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btton_add_Personaje.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btton_add_Personaje.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btton_add_Personaje.ForeColor = System.Drawing.Color.Yellow
+        Me.btton_add_Personaje.Image = CType(resources.GetObject("btton_add_Personaje.Image"), System.Drawing.Image)
+        Me.btton_add_Personaje.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btton_add_Personaje.Location = New System.Drawing.Point(3, 112)
+        Me.btton_add_Personaje.Name = "btton_add_Personaje"
+        Me.btton_add_Personaje.Size = New System.Drawing.Size(368, 103)
+        Me.btton_add_Personaje.TabIndex = 0
+        Me.btton_add_Personaje.Text = "Añadir Personaje"
+        Me.btton_add_Personaje.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btton_add_Personaje.UseVisualStyleBackColor = False
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.btton_Discard, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btton_Save, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(3, 452)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(416, 44)
+        Me.TableLayoutPanel1.TabIndex = 1
+        '
+        'btton_Save
+        '
+        Me.btton_Save.BackColor = System.Drawing.Color.Yellow
+        Me.btton_Save.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btton_Save.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btton_Save.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btton_Save.Location = New System.Drawing.Point(3, 3)
+        Me.btton_Save.Name = "btton_Save"
+        Me.btton_Save.Size = New System.Drawing.Size(202, 38)
+        Me.btton_Save.TabIndex = 0
+        Me.btton_Save.Text = "Guardar"
+        Me.btton_Save.UseVisualStyleBackColor = False
+        '
+        'btton_Discard
+        '
+        Me.btton_Discard.BackColor = System.Drawing.Color.Yellow
+        Me.btton_Discard.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btton_Discard.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btton_Discard.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btton_Discard.Location = New System.Drawing.Point(211, 3)
+        Me.btton_Discard.Name = "btton_Discard"
+        Me.btton_Discard.Size = New System.Drawing.Size(202, 38)
+        Me.btton_Discard.TabIndex = 1
+        Me.btton_Discard.Text = "Descartar"
+        Me.btton_Discard.UseVisualStyleBackColor = False
         '
         'writable_Form
         '
@@ -141,14 +186,18 @@ Partial Class writable_Form
         Me.tlp_Principal.ResumeLayout(False)
         Me.tlp_DisplayContent.ResumeLayout(False)
         Me.tlp_Buttons.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents tlp_Principal As TableLayoutPanel
     Friend WithEvents tlp_DisplayContent As TableLayoutPanel
-    Friend WithEvents WritableContent1 As WritableContentControl.WritableContent
+    Friend WithEvents wcontent As WritableContentControl.WritableContent
     Friend WithEvents tlp_Buttons As TableLayoutPanel
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents btton_add_Personaje As Button
+    Friend WithEvents btton_add_Director As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents btton_Save As Button
+    Friend WithEvents btton_Discard As Button
 End Class
