40f60bf033cd6a6e88175d372784aeb06d26a17b
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-01-14 15:29:00 +0100

    Integration of Forms

---
+++ Codigo/videoclub/videoclub/videoclub/Vista/Views_Form.vb
@@ -0,0 +1,9 @@
+﻿Public Class Views_Form
+    Shared formulario As Views_Form
+    Shared Function GetInstance() As Views_Form
+        If formulario Is Nothing Then
+            formulario = New Views_Form
+        End If
+        Return formulario
+    End Function
+End Class
--- Codigo/videoclub/videoclub/videoclub/Vista/writable_Form.vb
+++ Codigo/videoclub/videoclub/videoclub/Vista/writable_Form.vb
@@ -1,3 +1,11 @@
 ﻿Public Class writable_Form
 
+    Shared formulario As writable_Form
+    Shared Function GetInstance() As writable_Form
+        If formulario Is Nothing Then
+            formulario = New writable_Form
+        End If
+        Return formulario
+    End Function
+
 End Class
