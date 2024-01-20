b6fb9cc898debfa015d447c2e6287ad7678e7920
Luis Enrique Medina Murillo <luisenrique.medina@ikasle.egibide.org>
2024-01-20 12:51:40 +0100

    Add Method AddPeliculaAll

--- Codigo/videoclub/videoclub/videoclub/Controlador/WriteSQL.vb
+++ Codigo/videoclub/videoclub/videoclub/Controlador/WriteSQL.vb
@@ -51,6 +51,37 @@ Public Class WriteSQL
         End Try
     End Sub
 
+
+    Sub AddPelicula_All(nombre As String, duracion As String, productora As String, sinopsis As String, genero As String, director As Integer, poster As Image)
+        Dim con As SQLiteConnection = New SQLiteConnection(My.Settings.conexion_db)
+        Try
+            con.Open()
+            Dim query As String = String.Format("INSERT INTO PELICULAS (DIRECTOR,NOMBRE, DURACION, PRODUCTORA, GENERO, SINOPSIS, POSTER) VALUES (@director ,@nombre, @duracion,@productora,@genero,@sinopsis,@poster)")
+            Dim cmd As New SQLiteCommand(query, con)
+
+            cmd.Parameters.Add("@director", DbType.Int64).Value = director
+            cmd.Parameters.Add("@nombre", DbType.String).Value = nombre
+            cmd.Parameters.Add("@duracion", DbType.Int64).Value = duracion
+            cmd.Parameters.Add("@productora", DbType.String).Value = productora
+            cmd.Parameters.Add("@genero", DbType.String).Value = genero
+            cmd.Parameters.Add("@sinopsis", DbType.String).Value = sinopsis
+            If poster IsNot Nothing Then
+                Using stream As New MemoryStream()
+                    poster.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg)
+                    Dim imageData As Byte() = stream.ToArray
+                    cmd.Parameters.Add("@poster", DbType.Binary).Value = imageData
+                End Using
+            Else
+                Throw New Exception("Imagen sin valor")
+            End If
+            cmd.ExecuteNonQuery()
+
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+
+        End Try
+    End Sub
+
     Sub AddPelicula(nombre As String, duracion As String, productora As String, sinopsis As String, genero As String, director As Integer)
         Dim con As SQLiteConnection = New SQLiteConnection(My.Settings.conexion_db)
         Try
